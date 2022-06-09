87c8207a1f5fc17d6fed138e8998eee33ea8c32b
Peter Felis <Peter@felis.nl>
2022-06-09 11:56:26 +0200

    Update index.svelte
    werkt, alleen nog ghost probleem

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,52 +1,26 @@
 <script>
-
-	let lala = [];
-
 	let dingen = [
-		{ vnaam: 'Peter', anaam: 'Felis', id: 1, pos: 100 },
-		{ vnaam: 'Max', anaam: 'Felis', id: 2, pos: 200 },
-		{ vnaam: 'Petry', anaam: 'Botter', id: 3, pos: 300 },
-		{ vnaam: 'Stefanie', anaam: 'Felis', id: 4, pos: 400 },
-		{ vnaam: 'Liv', anaam: 'Botter', id: 5, pos: 500 }
+		{ vnaam: 'Peter', anaam: 'Felis', id: 1, pos: 1 },
+		{ vnaam: 'Max', anaam: 'Felis', id: 2, pos: 2 },
+		{ vnaam: 'Petry', anaam: 'Botter', id: 3, pos: 3 },
+		{ vnaam: 'Stefanie', anaam: 'Felis', id: 4, pos: 4 },
+		{ vnaam: 'Liv', anaam: 'Botter', id: 5, pos: 5 }
 	];
 
 	function drag(e) {
-		let hulp=[];
-		let scherm = document.querySelectorAll('.lijst .card');
-		scherm.forEach((item) => {
-			hulp.push({ y: item.offsetTop, id: item.getAttribute('data-id') });
-		});
-		lala=[...hulp];
-		
+		const scherm = document.querySelectorAll('.lijst .card');
+		for (let teller = 0; teller < scherm.length; teller++)
+			dingen[teller].pos = scherm[teller].offsetTop;
 	}
 
 	function dragend(e) {
 		let pos = e.clientY;
 		let id = e.target.getAttribute('data-id');
-
-		let teverwijderen = lala.findIndex((item) => item.id ==id )
-		let intevoegen = (lala.splice(teverwijderen,1));
-		let nieuwedingen=[];
-
-		
-		for (let teller = 0; teller<=lala.length; teller++) {
-			if (pos < lala[teller].y) {
-				lala.splice(teller,0,intevoegen[0]);
-				break;				
-			}
-		}
-		
-		
-		for (let teller=0; teller<lala.length; teller++){
-	 	nieuwedingen.push(dingen[lala[teller].id-1]);	
-		}			
-		console.log(nieuwedingen);
-
-		//console.log(dingen);
-		//console.log(intevoegen);
-		//let nieuwdingen = [...dingen];
-		dingen=[];
-		dingen = nieuwedingen;
+		let aanpassen = dingen.findIndex((item) => item.id == id);
+		dingen[aanpassen].pos = pos;
+		dingen.sort((a, b) => a.pos - b.pos);
+		for (let teller = 0; teller < dingen.length; teller++) dingen[teller].pos = teller + 1;
+		console.log(dingen);
 	}
 </script>
 
@@ -65,10 +39,6 @@
 </div>
 
 <style>
-	.raar {
-		background-color: aqua;
-		color: red;
-	}
 	.card {
 		border: 1px solid black;
 		border-radius: 4px;
